024d27e673165287d4809b26e20c8eb29c14e64e
geneo924 <emoore.dev@gmail.com>
2023-02-21 23:48:12 -0500

    add getter/setter methods

--- entryportal_spring/src/main/java/com/projects/entryportal/model/Job.java
+++ entryportal_spring/src/main/java/com/projects/entryportal/model/Job.java
@@ -17,4 +17,55 @@ public class Job {
     // job listing still active ? either return true or false
     private boolean active;
 
+    public Job(String id, String posting, String company, String description, boolean active) {
+        this.id = id;
+        this.posting = posting;
+        this.company = company;
+        this.description = description;
+        this.active = active;
+    }
+
+    public Job(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPosting() {
+        return posting;
+    }
+
+    public void setPosting(String posting) {
+        this.posting = posting;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
