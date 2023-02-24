0e1b4d7b4f3bf0cb896d20b74153f24e45e9848a
geneo924 <emoore.dev@gmail.com>
2023-02-23 23:14:35 -0500

    remove comments

--- entryserver/src/main/java/com/projects/entryportal/repository/JobRepository.java
+++ entryserver/src/main/java/com/projects/entryportal/repository/JobRepository.java
@@ -5,14 +5,6 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JobRepository extends MongoRepository<Job, String>{
-
     List<Job> findByPostingContaining(String post);
-    /*
-    can add these later...
-    findByJobDescription
-    findByActive
-    findByNewlyPosted
-
-     */
 }
 
