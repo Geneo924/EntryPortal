e71579ae989d7d73db6c5b15238de1c79a361561
geneo924 <genemoore924@gmail.com>
2023-03-07 11:00:42 -0500

    add route joblist

--- entryfe/src/App.js
+++ entryfe/src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddJob from "./components/add-job.component";
+import JobList from "./components/job-list.component";
 class App extends Component {
   render() {
- 
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            EntryPortal
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Job Listings
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add Job
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<JobList/>} />
+            <Route path="/tutorials" element={<JobList/>} />
+            <Route path="/add" element={<AddJob/>} />
+            
+          </Routes>
+        </div>
+      </div>
+    );
   }
 }
 
